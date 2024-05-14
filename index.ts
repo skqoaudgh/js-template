1bca0a6b905489ad43dd9bbb6320b54eb3c636f3
Bae Myungho <bmh961@naver.com>
2024-05-14 21:13:23 +0900

    feat: add 'attributes' property to event data

--- index.ts
+++ index.ts
@@ -1,23 +1,31 @@
 interface dataInterface {
 	tag: string;
 	label: string;
+	attributes: object;
 }
 
 const EVENTS: string[] = ['click'];
 
 const callback = (e: Event) => {
-	const labelArray: string[] = [];
 	const element = e.target as HTMLElement;
+	const { attributes: attrs } = element;
 
+	const labelArray: string[] = [];
 	for (const child of element.childNodes) {
 		if (child.nodeType === Node.TEXT_NODE) {
 			labelArray.push(child.nodeValue);
 		}
 	}
 
+	const attributes = Array.from(attrs).reduce((acc, { name, value }) => {
+		acc[name] = value;
+		return acc;
+	}, {});
+
 	const data: dataInterface = {
 		tag: element.tagName,
 		label: labelArray.join('\n'),
+		...Object.keys(attributes).length > 0 && { attributes },
 	};
 	
 	console.log(data);
