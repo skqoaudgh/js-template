34fa08def88e609e42d33a338ffc70190728eb7f
Bae Myungho <bmh961@naver.com>
2024-05-25 12:39:43 +0900

    feat: add initial files

--- src/constants.ts
+++
@@ -1,8 +0,0 @@
-const URL: string = 'http://localhost:3000';
-export const EVENT_POST_URL = `${URL}/events`;
-export const PAGE_VIEW_POST_URL = `${URL}/page-views`;
-export const PAGE_STAY_POST_URL = `${URL}`;
-
-export const COOKIE_NAME: string = '_buddy_id';
-
-export const MAX_PAYLOAD_SIZE: number = 5;
